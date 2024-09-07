ee1606f91d031450f02ee75f695ace69af2fc894
Muhammad Usman Shamim <hmusmanshamim99@gmail.com>
2024-09-07 23:43:44 +0500

    Add files via upload

--- main.dart
+++ main.dart
@@ -16,6 +16,8 @@
 
 // }
 
+// import 'dart:io';
+
 void main() {
   // Q.1: Check if a given number is positive, negative, or zero
   // int number = 0;
@@ -79,9 +81,134 @@ void main() {
   // }
 
   // // Q.8: Marksheet program
-  int subject1 = 85, subject2 = 90, subject3 = 75, subject4 = 88;
-  int totalMarks = subject1 + subject2 + subject3 + subject4;
-  double percentage = (totalMarks / (4 * 100)) * 100;
-  print("Total Marks: $totalMarks");
-  print("Percentage: $percentage%");
+  // int subject1 = 85, subject2 = 90, subject3 = 75, subject4 = 88;
+  // int totalMarks = subject1 + subject2 + subject3 + subject4;
+  // double percentage = (totalMarks / (4 * 100)) * 100;
+  // print("Total Marks: $totalMarks");
+  // print("Percentage: $percentage%");
+
+  // Q.9: Check if a given number is a palindrome
+  // int number = 121;
+  // int reversedNumber = 0;
+  // int originalNumber = number;
+  // while (originalNumber != 0) {
+  //   int remainder = originalNumber % 10;
+  //   reversedNumber = (reversedNumber * 10) + remainder;
+  //   originalNumber = originalNumber ~/ 10;
+  // }
+  // if (number == reversedNumber) {
+  //   print("$number is a palindrome.");
+  // } else {
+  //   print("$number is not a palindrome.");
+  // }
+
+  // stdin.readLineSync();
+
+  //Q.10 check correct ID and Password with if condition and given command wether
+  // correct or not
+  // 'dart:io';
+
+  // String email = "admin@ex.com";
+  // String password = "1234";
+
+  // print("Enter your email:");
+  // var inputEmail = stdin.readLineSync();
+
+  // print("Enter your password:");
+  // var inputPassword = stdin.readLineSync();
+
+  // if (email == inputEmail && password == inputPassword) {
+  //   print("Login Successful");
+  // } else {
+  //   print("Invalid Email or Password");
+  // }
+  // List hell = ["hallu", "codewithusman", 0312, 557];
+  // print(hell[2]);
+
+  // List mylist = [0, "gggg", 546, "uhd", 1234];
+  // mylist[3] = 999;
+  // var list = List.of(mylist.reversed);
+  // print(mylist);
+  // print('after reversed : $list');
+
+  // var data = {
+  //   "name": "usman",
+  //   "age": 21,
+  //   "city": "Karachi",
+  //   "occupation": "Web-Develper",
+  // };
+  // print(data);
+  // data.clear();
+  // print(data);
+
+  print("Muhammad Usman Shamim");
+  print("Assignment # 03");
+  print("Question # 01 \n");
+
+  List names = ["Usman", "Summiya", "Rafia", "Misbah", "Afeefah", "Shaffan"];
+  print(names);
+
+  print("Question # 02 \n");
+
+  List Days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thrusday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+  ];
+  print(Days[6]);
+
+  print("Question # 03 \n");
+
+  List Student = [
+    "Name : " "Usman",
+    "Class : " "14th",
+    "Roll no : " "3058-2023",
+    "Grade : " "A+",
+    "Percentage :" "88.45"
+  ];
+  print(Student);
+
+  print("Question # 04 \n");
+
+  List integers = [82, 32, 67, 34, 89, 58, 44];
+  int smallest = integers.reduce((a, b) => a < b ? a : b);
+  int greatest = integers.reduce((a, b) => a > b ? a : b);
+
+  print('Smallest number: $smallest');
+  print('Greatest number: $greatest');
+
+  print("Question # 05 \n");
+
+  List<int> numbers = [10, 24, 53, 18, 35];
+
+  int maxNumber = numbers.reduce((a, b) => a > b ? a : b);
+
+  print('Maximum value: $maxNumber');
+
+  print("Question # 06 \n");
+
+  List string = ["Usman", "Shaffan", "Faizan", "Afeefah"];
+  print(string);
+  var list = List.of(string.reversed);
+  print("After reversed $list");
+
+  print("Question # 07 \n");
+
+  List number = [12, -4, 18, -7, 5, -13, 9, 34];
+  List positiveNumbers = number.where((num) => num >= 0).toList();
+
+  print('Original List: $number');
+  print("Positive Numbers: $positiveNumbers");
+
+  print("Question # 08 \n");
+
+  List usersEligibility = ['John', 'Alice', 'eligible', 'Mike', 'Sarah', 'Tom'];
+
+  usersEligibility.removeWhere((user) => user != "eligible");
+
+  print("Filtered List: $usersEligibility");
 }
