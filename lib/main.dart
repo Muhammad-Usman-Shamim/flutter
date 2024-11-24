38c126f3c4d7361ef4894f36cb212af7f4172328
Muhammad Usman Shamim <hmusmanshamim99@gmail.com>
2024-11-24 22:09:44 +0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:medcare/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomeScreen(),
+    );
+  }
+}
